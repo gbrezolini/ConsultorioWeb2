e976813a061410a1c823d8244c7d1dd4f284985b
gbrezolini <gbrezolini@yahoo.com.br>
2020-11-30 10:40:14 -0300

    First commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System.Web.Mvc;
+
+namespace ConsultorioWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Sistema de gerenciamento de clínica odontológica";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Equipe de Desenvolvimento";
+
+            return View();
+        }
+    }
+}
---
+++ Controllers/PacienteController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ConsultorioWeb.Models;
+
+namespace ConsultorioWeb.Controllers
+{
+    public class PacienteController : Controller
+    {
+        private DentalSystemEntities db = new DentalSystemEntities();
+
+        // GET: Paciente
+        public ActionResult Index()
+        {
+            return View(db.Paciente.ToList());
+        }
+
+        // GET: Paciente/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Paciente paciente = db.Paciente.Find(id);
+            if (paciente == null)
+            {
+                return HttpNotFound();
+            }
+            return View(paciente);
+        }
+
+        // GET: Paciente/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Paciente/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Nome,Cpf,Rg,Endereco,Numero,Bairro,Telefone,DataNascimento,Profissao,Email,UF,Cidade,EstadoCivil,Convenio,NomeConjuge,TelefoneConjuge")] Paciente paciente)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Paciente.Add(paciente);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(paciente);
+        }
+
+        // GET: Paciente/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Paciente paciente = db.Paciente.Find(id);
+            if (paciente == null)
+            {
+                return HttpNotFound();
+            }
+            return View(paciente);
+        }
+
+        // POST: Paciente/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Nome,Cpf,Rg,Endereco,Numero,Bairro,Telefone,DataNascimento,Profissao,Email,UF,Cidade,EstadoCivil,Convenio,NomeConjuge,TelefoneConjuge")] Paciente paciente)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(paciente).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(paciente);
+        }
+
+        // GET: Paciente/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Paciente paciente = db.Paciente.Find(id);
+            if (paciente == null)
+            {
+                return HttpNotFound();
+            }
+            return View(paciente);
+        }
+
+        // POST: Paciente/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Paciente paciente = db.Paciente.Find(id);
+            db.Paciente.Remove(paciente);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
